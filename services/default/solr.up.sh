95ed48d95a10ca0c32052b729794041eea50f40a
Dimitris Bozelos <dbozelos@gmail.com>
2024-02-11 19:25:12 +0000

    Support Solr versions higher than 7

--- services/default/solr.up.sh
+++ services/default/solr.up.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+version_major=${APP_CONTAINER_SOLR_VERSION:0:1}
+
+volume_path=/var/solr/data
+# Compatibility with previous versions of the docker image.
+if [ $version_major = "7" ]
+then
+   volume_path=/opt/solr/server/solr/mycores
+fi
+
 p run \
   --detach \
   --pod ${APP_CONTAINER_POD_NAME} \
   --name ${APP_CONTAINER_POD_NAME}_solr \
-  --volume ${APP_CONTAINER_VOLUMES_DIR}/solr:/var/solr \
-  --volume ${PWD}/podman/solr-conf:/opt/solr/server/solr/configsets/app:ro \
-  docker.io/solr:${APP_CONTAINER_SOLR_VERSION} solr-precreate app /opt/solr/server/solr/configsets/app
+  --env JAVA_OPTS="-Xms1024m -Xmx2048m" \
+  --volume ${APP_CONTAINER_VOLUMES_DIR}/solr:$volume_path \
+  docker.io/solr:${APP_CONTAINER_SOLR_VERSION} solr-precreate ${APP_CONTAINER_SOLR_CORE}
