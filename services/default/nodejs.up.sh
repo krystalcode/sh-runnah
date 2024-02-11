aa29514bbe46fe6c92f385ffe54d776e16375efd
Dimitris Bozelos <dbozelos@gmail.com>
2024-02-11 19:22:29 +0000

    Added service for nodejs

---
+++ services/default/nodejs.up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+p run \
+  --detach \
+  --pod ${APP_CONTAINER_POD_NAME} \
+  --name ${APP_CONTAINER_POD_NAME}_$service \
+  --volume ${PWD}:/src \
+  --workdir /src \
+  docker.io/krystalcode/f_nvm sleep infinity
