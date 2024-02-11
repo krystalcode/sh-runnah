1093f87a53bfe5a3b0ba1ee098cfe0adf8d49c33
Dimitris Bozelos <dbozelos@gmail.com>
2024-02-11 19:23:41 +0000

    Added service for haskell stack

---
+++ services/default/stack.up.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SERVICE=$1
+BUILDAHFILE=$2
+
+if [ -z ${BUILDAHFILE} ]; then
+    image=docker.io/krystalcode/f_haskell_stack:latest
+else
+    image=${APP_CONTAINER_REGISTRY}/${APP_CONTAINER_POD_NAME}_${SERVICE}
+fi
+
+p run \
+  --detach \
+  --pod ${APP_CONTAINER_POD_NAME} \
+  --name ${APP_CONTAINER_POD_NAME}_${SERVICE} \
+  --volume ${PWD}:/src \
+  --workdir /src \
+  $image \
+  sleep infinity
