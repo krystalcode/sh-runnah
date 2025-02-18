b1eac5f8615337ac6e220e13fadb076ce3add7a0
Dimitris Bozelos <dbozelos@gmail.com>
2025-02-18 01:09:19 +0000

    Used the image variable provided to the up script by the env builder

--- services/default/syncthing.up.sh
+++ services/default/syncthing.up.sh
@@ -5,16 +5,11 @@ buildahfile=$2
 run_command=$3
 image_name=$4
 image_version=$5
-
-if [ -z ${buildahfile} ]; then
-    image="${image_name}:${image_version}"
-else
-    image="${APP_CONTAINER_REGISTRY}/${APP_CONTAINER_POD_NAME}_${service}"
-fi
+image=$6
 
 read -r -d '' run_command_addition << COMMAND
   --volume ${APP_CONTAINER_VOLUMES_DIR}:/var/syncthing \
-  docker.io/syncthing/syncthing
+  ${image}
 COMMAND
 run_command="${run_command} ${run_command_addition}"
 
