8e7020eadef323580bb413cf99d196ea97eda8dc
Dimitris Bozelos <dbozelos@gmail.com>
2025-02-18 01:08:48 +0000

    Added default service variables for varnish

--- services/default/varnish.up.sh
+++ services/default/varnish.up.sh
@@ -3,12 +3,15 @@
 service=$1
 buildahfile=$2
 run_command=$3
+image_name=$4
+image_version=$5
+image=$6
 
 read -r -d '' run_command_addition << COMMAND
   --env VARNISH_HTTP_PORT=7777
   --tmpfs /var/lib/varnish/varnishd:exec
   --volume ${APP_CONTAINER_VOLUMES_DIR}/varnish/config/default.vcl:/etc/varnish/default.vcl:ro
-  docker.io/varnish:${APP_CONTAINER_VARNISH_VERSION} -p http_resp_hdr_len=24k
+  ${image} -p http_resp_hdr_len=24k
 COMMAND
 run_command="${run_command} ${run_command_addition}"
 
