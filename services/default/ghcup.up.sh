f131192a2d3a91b0485ba39c0388acd53affbe77
Dimitris Bozelos <dbozelos@gmail.com>
2025-04-19 03:08:28 +0000

    Added service for Haskell's GHCUP

---
+++ services/default/ghcup.up.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+service=$1
+buildahfile=$2
+run_command=$3
+image_name=$4
+image_version=$5
+image=$6
+
+read -r -d '' run_command_addition << COMMAND
+  ${image} \
+  sleep infinity
+COMMAND
+run_command="${run_command} ${run_command_addition}"
+
+eval ${run_command}
