db7bc0572cca7715cfe89082b8d27594c61b311f
Dimitris Bozelos <dbozelos@gmail.com>
2025-04-23 06:11:04 +0000

    Updated ngrok service to use the latest agent

--- services/default/ngrok.up.sh
+++ services/default/ngrok.up.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
-p run \
-  --detach \
-  --pod ${APP_CONTAINER_POD_NAME} \
-  --name ${APP_CONTAINER_POD_NAME}_ngrok \
-  --env NGROK_AUTH=${APP_CONTAINER_NGROK_AUTH} \
-  docker.io/wernight/ngrok
+service=$1
+buildahfile=$2
+run_command=$3
+image_name=$4
+image_version=$5
+image=$6
+
+deprecated_variable_name="APP_CONTAINER_NGROK_AUTH"
+if [ ! -z ${!deprecated_variable_name+x} ]; then
+    echo "Please update your environment file to use APP_CONTAINER_NGROK_ENV__NGROK_AUTHTOKEN instead of APP_CONTAINER_NGROK_AUTH"
+    exit 1
+fi
+
+deprecated_variable_name="APP_CONTAINER_NGROK_ENV__NGROK_AUTH"
+if [ ! -z ${!deprecated_variable_name+x} ]; then
+    echo "Please update your environment file to use APP_CONTAINER_NGROK_ENV__NGROK_AUTHTOKEN instead of APP_CONTAINER_NGROK_ENV__NGROK_AUTH"
+    exit 1
+fi
+
+# If we are given a configuration file, we expect endpoints to be defined in it
+# and we start all of them.
+config_file="APP_CONTAINER_NGROK_CONFIG_FILE"
+if [ ! -z ${!config_file+x} ]; then
+    read -r -d '' run_command_addition << COMMAND
+      --volume ${APP_CONTAINER_NGROK_CONFIG_FILE}:/home/ngrok/.config/ngrok/ngrok.yml
+      ${image}
+      start --all --config /home/ngrok/.config/ngrok/ngrok.yml
+COMMAND
+# Otherwise, we run an endpoint on the given or default host/port.
+else
+    host_port="${APP_CONTAINER_NGROK_HOST}:${APP_CONTAINER_NGROK_PORT}"
+    read -r -d '' run_command_addition << COMMAND
+      ${image}
+      http ${host_port}
+COMMAND
+fi
+
+run_command="${run_command} ${run_command_addition}"
+eval ${run_command}
