184fba4b83a9394ed623eb613cd0c0b35b6e77b1
Dimitris Bozelos <dbozelos@gmail.com>
2025-08-20 20:11:41 -0500

    Use the already build variable with the image path for fedora

--- services/default/fedora.up.sh
+++ services/default/fedora.up.sh
@@ -3,11 +3,14 @@
 service=$1
 buildahfile=$2
 run_command=$3
+image_name=$4
+image_version=$5
+image=$6
 
 read -r -d '' run_command_addition << COMMAND
   --volume ${PWD}:/workdir
   --workdir /workdir
-  ${APP_CONTAINER_REGISTRY}/${APP_CONTAINER_POD_NAME}_fedora
+  ${image}
   sleep infinity
 COMMAND
 run_command="${run_command} ${run_command_addition}"
