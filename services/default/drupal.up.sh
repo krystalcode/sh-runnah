e1a286147caf2c4324057d8d23eb50c129998528
Dimitris Bozelos <dbozelos@gmail.com>
2025-09-07 18:55:38 -0500

    Added service for Drupal apache-based image

---
+++ services/default/drupal.up.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+service=$1
+buildahfile=$2
+run_command=$3
+image_name=$4
+image_version=$5
+image=$6
+
+backups_volume=""
+if [ ! -v ${APP_CONTAINER_APACHE_BACKUPS_DIR+x} ]; then
+    backups_volume="--volume ${APP_CONTAINER_APACHE_BACKUPS_DIR}:${APP_SCRIPTS_LOCAL_DIR}/rebuild/backups"
+fi
+
+read -r -d '' run_command_addition << COMMAND
+  --entrypoint /bin/bash
+  --tmpfs /app/cache:rw,noexec
+  --volume ${PWD}:/var/www/html
+  --volume ${PODMAN_VOLUMES_DIR}/global/php/.composer:/root/.composer
+  --volume ${PODMAN_VOLUMES_DIR}/global/php/.drush:/root/.drush
+  ${backups_volume}
+  ${image}
+  -c 'p && exec apache2-foreground'
+COMMAND
+run_command="${run_command} ${run_command_addition}"
+
+eval ${run_command}
